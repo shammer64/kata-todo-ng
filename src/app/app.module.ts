45dbb13be638b581dcee24263d1d0f08cb264190
Scott Hammer <scott.a.hammer@outlook.com>
2022-05-12 14:20:20 -0400

    SH Generated ToDoListComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ToDoListEntryComponent } from './to-do-list-entry/to-do-list-entry.component';
 import { ToDoListItemsComponent } from './to-do-list-items/to-do-list-items.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToDoListEntryComponent,
-    ToDoListItemsComponent
+    ToDoListItemsComponent,
+    ToDoListComponent
   ],
   imports: [
     BrowserModule
