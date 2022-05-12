1be0bcc9aaf9d3f32fb94fe50cc683a87b17e4a9
Scott Hammer <scott.a.hammer@outlook.com>
2022-05-12 14:14:22 -0400

    SH Generated ToDoListItemsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ToDoListEntryComponent } from './to-do-list-entry/to-do-list-entry.component';
+import { ToDoListItemsComponent } from './to-do-list-items/to-do-list-items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToDoListEntryComponent
+    ToDoListEntryComponent,
+    ToDoListItemsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/to-do-list-items/to-do-list-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-list-items',
+  templateUrl: './to-do-list-items.component.html',
+  styleUrls: ['./to-do-list-items.component.css']
+})
+export class ToDoListItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
