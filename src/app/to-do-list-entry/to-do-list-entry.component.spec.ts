5c8870259b14a81766bd3ef0ee6f3afca093376e
Scott Hammer <shammer64@gmail.com>
2022-05-13 11:29:25 -0400

    SH Added clearing of text field after submit

--- src/app/to-do-list-entry/to-do-list-entry.component.spec.ts
+++ src/app/to-do-list-entry/to-do-list-entry.component.spec.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 
@@ -29,26 +29,29 @@ describe('ToDoListEntryComponent', () => {
 
   it('should render a text entry field', () => {
     const htmlComponent = fixture.nativeElement as HTMLElement;
+
     expect(htmlComponent.querySelector('.todo-entry-text')).toBeTruthy();
   });
   
   it('should label the text entry field', () => {
     const htmlComponent = fixture.nativeElement as HTMLElement;
+
     expect(htmlComponent.querySelector('label')?.innerText).toBe('Please enter a new To-Do item:')
   });
 
   it('should render a "Submit" button', () => {
     const htmlComponent = fixture.nativeElement as HTMLElement;
     const button = htmlComponent.querySelector('.todo-entry-button');
+
     expect(button).toBeTruthy();
     expect(button?.innerHTML).toBe('Submit');
   });
 
   it('should update component property when text is entered', () => {
     const input = fixture.nativeElement.querySelector('.todo-entry-text');
+
     input.value = 'Mow the lawn';
     input.dispatchEvent(new Event('input'));
- 
     fixture.detectChanges();
 
     expect(component.newToDoItem).toEqual('Mow the lawn');
@@ -56,16 +59,18 @@ describe('ToDoListEntryComponent', () => {
 
   it('should disable "Submit" button until text is entered', () => {
     const button = fixture.nativeElement.querySelector('.todo-entry-button');
+
     expect(button.disabled).toBeTruthy();
   });
 
   it('should enable "Submit" button after text is entered', () => {
     const button = fixture.nativeElement.querySelector('.todo-entry-button');
     const input = fixture.nativeElement.querySelector('.todo-entry-text');
+
     input.value = 'Mow the lawn';
     input.dispatchEvent(new Event('input'));
-
     fixture.detectChanges();
+
     expect(button.disabled).toBeFalsy();
   });
 
@@ -73,13 +78,33 @@ describe('ToDoListEntryComponent', () => {
     spyOn(component, 'handleNewToDoItem');
     const button = fixture.nativeElement.querySelector('.todo-entry-button');
     const input = fixture.nativeElement.querySelector('.todo-entry-text');
+
     input.value = 'Mow the lawn';
     input.dispatchEvent(new Event('input'));
-
     fixture.detectChanges();
     button.click();
     tick();
 
     expect(component.handleNewToDoItem).toHaveBeenCalled();
   }));
+
+  it('should clear text when handler is invoked', fakeAsync(() => {
+    const button = fixture.nativeElement.querySelector('.todo-entry-button');
+    const input = fixture.nativeElement.querySelector('.todo-entry-text');
+
+    input.value = 'Mow the lawn';
+    input.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    tick();
+    // Either event trigger this action
+    // input.dispatchEvent(new Event('change'));
+    button.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+    tick();
+
+    expect(input.value).toBe('');
+  }));
+
+  xit('should invoke handler when text present and enter key is pressed');
+  xit('should emit custom event for parent');
 });
--- src/app/to-do-list-entry/to-do-list-entry.component.ts
+++ src/app/to-do-list-entry/to-do-list-entry.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoListEntryComponent implements OnInit {
   handleNewToDoItem() {
-    console.log("Button clicked")
+    console.log("Button clicked: " + this.newToDoItem);
+    this.newToDoItem = '';
   }
   newToDoItem: string = '';
 
